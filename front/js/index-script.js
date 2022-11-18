f23a9d35fff9026d73a6779bdc9e640ec503c3e1
Diego JULIA <106588200+HolyPandaRoux@users.noreply.github.com>
2022-11-18 09:07:43 +0100

    working index

--- front/js/index-script.js
+++ front/js/index-script.js
@@ -1,44 +1,45 @@
-/* Function to get the data from the server with fetch then formating the data receive then use the data then display the data using innerHtml */
+/* script pour récupérer les données du serveur par le biais de Fetch et stockage de celle ci dans la variable Returned Answer avant réutilisation*/
+// Fetch
+// response => Json
+// Json => table 
+// module d'erreur
+// fonction d'affichage des produits
+// displayZone correspond à la const/let qui contiendra l'affichage des produits
+//let article of index, 
+//rajout au html existant des éléments article de l'index avec en rajout le selector correspondant à la ligne de tableau voulue.
 
+fetch  ("http://192.168.1.60:3000/api/products")
+.then(response => response.json())
+// formatage des données au format json
+.then((ProductsTable)=> {
+// formatage des données sous forme de tableau pour une question de lisibilité
+    console.table(ProductsTable);
+    allItems(ProductsTable);
+})
+.catch((error ) => {
+// en cas d'erreur un message d'erreur 404 apparait sur le bloc faisant défaut plus mentions des sources potentielless d'erreur dans le console.log
+document.querySelector(".titles").innerHTML = "<h3> erreur 404 failed to get data from API check console log</h3>";
+console.log("erreur 404 Vérifier le statut du serveur /  fonction fetch / api data"+er);
+});
 
-
-fetch('http://192.168.1.60:3000/api/products')
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('HTTP error: ${response.status}');
-            //si erreur affichage dans la console " erreur de connection au serveur, vérifier statut server et/ou url du serveur
-        }
-        return response.json();
-    })
-    .then(json => initialize(json))
-    // transfert des données depuis l'api et versement dans JSON
-    .catch(err => console.error(`Fetch problem: ${err.message}`));
-// si erreur affichage dans la console "erreur de transfert des données vers JSON vérifier statut du fetch "
-function initialize(products) {
-    // grab the UI elements that we need to manipulate
-    let category = document.querySelector('#category');
-    let searchTerm = document.querySelector('#searchTerm');
-    let searchBtn = document.querySelector('button');
-    let main = document.querySelector('main');
-
-    // keep a record of what the last category and search term entered were
-    let lastCategory = category.value;
-    // no search has been made yet
-    let lastSearch = '';
-
-    // these contain the results of filtering by category, and search term
-    // finalGroup will contain the products that need to be displayed after
-    // the searching has been done. Each will be an array containing objects.
-    // Each object will represent a product
-    let categoryGroup;
-    let finalGroup;
-
-    // To start with, set finalGroup to equal the entire products database
-    // then run updateDisplay(), so ALL products are displayed initially.
-    finalGroup = products;
-    updateDisplay();
-
-    // Set both to equal an empty array, in time for searches to be run
-    categoryGroup = [];
-    finalGroup = [];
+function allItems(index) {
+// https://chartio.com/learn/databases/how-does-indexing-work/
+    let displayZone = document.querySelector("#items");
+// pour ( chaque article de index )
+    for (let article of index) {
+//https://www.w3schools.com/js/js_loop_forin.asp explications de l'utilisation de la loop " for Variable in Array"
+        displayZone.innerHTML +=
+//https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Operators/Addition_assignment 
+// lien explicatif sur le += concaténation ou addition avec les deux operands de gauche
+        `
+        <a href="./product.html?_id=${article._id}">
+            <article>
+                <img src="${article.imageUrl}" alt="${article.altTxt}">
+                <h3  class="productName">${article.name}</h3>
+                <h2  class="productPrice">${article.price}€</h2>
+                <p   class="productDescription">${article.description}                                       
+                </p>
+            </article>
+        </a>`;
+    }
 }
