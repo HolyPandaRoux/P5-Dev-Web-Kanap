d54a08a9c7ae93ef968682266cacb0308f2c2a04
Diego JULIA <106588200+HolyPandaRoux@users.noreply.github.com>
2022-11-20 16:58:27 +0100

    Update productcall.js

--- front/js/productcall.js
+++ front/js/productcall.js
@@ -1,5 +1,5 @@
 
-document.write(document.location)
+window.write(window.location)
 // récupération de l'url actuelle de la page affiché
 // source : https://developer.mozilla.org/fr/docs/Web/API/Document/location
 if((err) => {
@@ -9,21 +9,23 @@ if((err) => {
 console.log('url=',document.location)
 //affichage dans console log du résultat
 const parsedUrl = new URL(window.location.href);
-let id = (parsedUrl.searchParams.get("_id"))
-console.log('id=',id);
-console.log(id);
-let idProduit = parsedUrl.searchParams.get("products");
-// affich1age dans console log du résultat
-// récupération de l'id affiché à l'écran ( qui correspond au produit sur lequel l'utilisateur à cliqué)
-// source : https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams/get
+let newParams = (parsedUrl.searchParams.get("_id"))
+console.log('id=',id)
+ let ProductPage = new URL (`http://192.168.1.63:5500/front/html/product.html?_id=${id}}`)
 
-    //let response = fetch(`http://192.168.1.63:5500/front/html/product.html?${id}`)
-    //const produtcs = (await response).json();
-    //console.log(produtcs); 
+// récupération de l'id affiché à l'
+écran ( qui correspond au produit sur lequel l'utilisateur à cliqué)
+
+
+//let response = fetch(`http://192.168.1.63:5500/front/html/product.html?${id}`)
+//const produtcs = (await response).json();
+//console.log(produtcs); 
 
 //loadNames();
 
-
+//let myUsername = "someguy";
+//let addr = new URL("https://example.com/login");
+//addr.username = myUsername;
 
 
 
