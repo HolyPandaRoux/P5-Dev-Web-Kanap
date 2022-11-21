f324c9f6834908e6b2f42a0f23c0c4293df970db
Diego JULIA <106588200+HolyPandaRoux@users.noreply.github.com>
2022-11-21 01:50:33 +0100

    product

--- front/js/productcall.js
+++ front/js/productcall.js
@@ -2,37 +2,33 @@
 
 const parsedUrl = new URL(window.location.href);
 let id = (parsedUrl.searchParams.get("_id"));
-console.log('id=',id);
+console.log('id=', id);
 // affichage dans console log du résultat
 // récupération de l'id affiché à l'écran ( qui correspond au produit sur lequel l'utilisateur à cliqué)
 // source : https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams/get
 // récupération de l'id affiché à l'
 
-fetch  (`http://localhost:3000/api/products/${id}`)
-.then(response => response.json())
-// formatage des données au format json
-.then((product)=> {
-// formatage des données sous forme de tableau pour une question de lisibilité
-    console.table(product);
-})
-.catch((error ) => {
-// en cas d'erreur un message d'erreur 404 apparait sur le bloc faisant défaut plus mentions des sources potentielless d'erreur dans le console.log
-document.querySelector(".titles").innerHTML = "<h3> erreur 404 failed to get data from API check console log</h3>";
-console.log("erreur 404 Vérifier le statut du serveur /  fonction fetch / api data"+er);
-});
-//let response = fetch(`http://192.168.1.63:5500/front/html/product.html?${id}`)
-//const produtcs = (await response).json();
-//console.log(produtcs); 
+fetch(`http://localhost:3000/api/products/${id}`)
+    .then(response => response.json())
+    // formatage des données au format json
+    .then((product) => {
+        // formatage des données sous forme de tableau pour une question de lisibilité
+        console.table(product);
+    })
+    .catch(() => {
+        // en cas d'erreur un message d'erreur 404 apparait sur le bloc faisant défaut plus mentions des sources potentielless d'erreur dans le console.log
+        console.log("erreur 404 Vérifier le statut du serveur /  fonction fetch / api data" + er);
+    });
 
-//loadNames();
+for 'product' in id (element => {
+    function affichageproduit(_id) {
+        let name = document.getElementById('#title').innerHTML;
+        let description = document.getElementById('#description').innerHTML;
+        let price = document.getElementById('#price').innerHTML;
+        let rating = document.getElementById('#rating').innerHTML;
+        let url = document.getElementById('url+id').innerHTML;
+        let imageUrl = document.getElementById('#imageUrl');
 
-//let myUsername = "someguy";
-//let addr = new URL("https://example.com/login");
-//addr.username = myUsername;
-
-
-
-
-
-
-
+    
+	}}
+);  
