fe6c4238f1c415d8b59e97f53e3af364c6490206
Diego JULIA <diego.julia@icloud.com>
2022-12-01 16:09:33 +0100

    data from cart + api ok  in console.log

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,25 +1,25 @@
 function getPanier() { 
-return localStorage.getItem('cart');
-}
+    //getting the data from the local storage it will come as a string so we need to parse it 
+return localStorage.getItem('cart');}
 let panier = getPanier();
-panier = JSON.parse(panier);
+    // parsing the response from JSON
+    panier = JSON.parse(panier);
+    console.table(panier);
+    // getting all the items in the cart and displaying them in the console as a table
+let allProducts = [];
+    // iterating through the cart to get the id of each product selected by the customer    
+    for (let i = 0; i < panier.length; i++) {
+    // if you want to get all the id present in panier displayed use this => console.log(panier[i].id);
+        let id = panier[i].id;
+    // using the id we got from the panier(cart) to fetch the data from the api using the url and the ${id} and pushing it to allProducts
+        fetch(`http://localhost:3000/api/products/${id}`)
+            .then(response => response.json())
+            .then((product) => {
+                console.table(product);
+                allProducts.push(product);
+            });
+    }
 
-let allProducts =  [] ;
-fetch ("http://localhost:3000/api/products")
-.then(response => response.json())
-.then((ProductsTable) => {
-    console.table(ProductsTable);
-    allProducts = ProductsTable;
 
-});
 
-function getData (){
-let cartItems = [];
-for (let i = 0; i < panier.length; i++) {
-    let id = panier[i].id;
-    let product = allProducts.find((product) => product._id == id);
-    cartItems.push(product);
-}
-console.log(cartItems);
 
-}
