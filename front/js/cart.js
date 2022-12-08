947e65f08bf0730e38383e96d8b20f614ec6ec13
Diego JULIA <106588200+HolyPandaRoux@users.noreply.github.com>
2022-12-08 17:18:48 -0500

    cart

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,16 +1,16 @@
-let allProducts = [];
-
+let allProducts     = [];
+const cartSection   = document.getElementById        ("cart__items");
+const cartOrder     = document.getElementsByClassName("cart__order");
+const cartPrice     = document.getElementsByClassName("cart__price");
+const h1            = document.getElementsByTagName  ("h1");
 
 function getPanier() {
     return localStorage.getItem('cart');
 }
 
-
 let panier = getPanier();
 panier = JSON.parse(panier);
 console.table(panier);
-
-
 
 async function getAllProducts() {
     for (let i = 0; i < panier.length; i++) {
@@ -32,90 +32,51 @@ async function getProductsId() {
     for (let i = 0; i < index.length; i++) {
         console.log(index[i]);
     }
-    
+}
 getProductsId();
 
 
-
-
-
-
-
-
-
-
-function displayCart(fullCart) {
-    const displayZone = document.querySelector("#cart__items");
-    displayZone.innerHTML +=
-        `<article class="cart__item" data-id="${fullCart.id}" data-color="${fullCart.color}" data-quantity="${fullCart.quantity}" data-price="${fullCart.price}"> 
-            <div class="cart__item__img">
-                <img src="${fullCart.imageUrl}" alt="${fullCart.altTxt}">
-            </div>
-            <div class="cart__item__content">
-                <div class="cart__item__content__description">
-                    <h2>${fullCart.name}</h2>
-                    <span>Couleur : ${fullCart.color}</span>
-                    <p data-price="${fullCart.price}">Prix : ${fullCart.price} €</p>
-                </div>
-                <div class="cart__item__content__settings">
-                    <div class="cart__item__content__settings__quantity">
-                        <p>Quantité : </p>
-                        <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${fullCart.quantity}">
+function fetchIdData() {
+    let items = getPanier();
+    let quantity = 0;
+    let price = 0;
+    if (localStorage.getItem("panier") != null) {
+    for (let i = 0; i < items.length; i++) {
+        let id = items[i][0];
+        let color = items[i][1];
+        let url = host + "api/products/" + id;
+        fetch(url)
+            .then((response) => response.json())
+            .then((data) => {
+                cartSection.innerHTML += `<article class="cart__item" data-id="${id}" data-color="${color}">
+                    <div class="cart__item__img">
+                    <img src="${data.imageUrl}" alt="${data.altTxt}">
                     </div>
-                    <div class="cart__item__content__settings__delete">
-                    <p class="deleteItem" data-id="${fullCart.id}" data-color="${fullCart.color}">Supprimer</p>
+                    <div class="cart__item__content">
+                    <div class="cart__item__content__titlePrice">
+                        <h2>${data.name}</h2>
+                        <p>${color}</p>
+                        <p>${data.price} €</p>
                     </div>
-                </div>
-            </div>
-        </article>
-        `
-
-};
-
-
-
-
-
-/*
-let cartName = allProducts.name;
-let cartDescription = allProducts.description;
-let cartPrice = allProducts.price;
-let cartUrl = allProducts.imageUrl;
-let cartAltTxt = allProducts.altTxt;
-let cartId = allProducts._id.valueOf ;
-let cartQuantity = getPanier.quantity;
-let cartColor = getPanier.color;
-console.log(cartName, cartDescription, cartPrice, cartUrl,cartAltTxt, cartId, cartQuantity, cartColor);
-$fullCart = array(
-    "allProducts[i].name" = "selectedName",
-    "allProducts[i].description" = "selectedDescription",
-    "allProducts[i].price" = "selectedPrice",
-    "allProducts[i].imageUrl" = "selectedImageUrl",
-    "allProducts[i].altTxt" = "selectedAltTxt",
-    "allProducts[i]._id" = "selectedId",
-    "panier[i].quantity" = "quantity",
-    "panier[i].color" = "color",
-)
-print_r($array),
-console.log(fullCart);
-    
-
-
-
-    
-let fullCart = [];
-    for (let i = 0; i < allProducts.length; i++) {
-        let selectedName                    = allProducts[i].name;
-        let description			            = allProducts[i].description;
-        let price 							= allProducts[i].price;
-        let imageUrl 						= allProducts[i].imageUrl;
-        let altTxt 							= allProducts[i].altTxt;
-        let quantity                        = panier[i].quantity;
-        let color                           = panier[i].color;
-        let id                              = allProducts[i]._id;
-    fullCart.push(selectedName, description, price, imageUrl, altTxt, quantity, color,id)
-    console.log(fullCart);
-
-    
+                    <div class="cart__item__content__settings">
+                        <div class="cart__item__content__settings__quantity">
+                        <p>Qté :${quantity} </p>
+                        <input type="number" class="itemQuantity" name="itemQuantity" onchange="changeQuantity('${id}', '${color}', this.value)" min="1" max="100" value="${items[i][2]}">
+                        </div>
+                        <div class="cart__item__content__settings__delete">
+                        <p class="deleteItem" onclick="deleteItem('${id}','${color}')">Supprimer</p>
+                        </div>
+                    </div>
+                    </div>
+                </article>`;
+            // total price (if qty (items[i][2]))
+            price += data.price * items[i][2];
+            document.getElementById("totalPrice").innerHTML = price;
+            });
+        }
     }
-    */
+}
+
+
+
+
--- front/js/cartTest.js
+++ front/js/cartTest.js
@@ -1,3 +1,101 @@
+
+
+function displayCart(fullCart) {
+    const displayZone = document.querySelector("#cart__items");
+    displayZone.innerHTML +=
+        `<article class="cart__item" data-id="${fullCart.id}" data-color="${fullCart.color}" data-quantity="${fullCart.quantity}" data-price="${fullCart.price}"> 
+            <div class="cart__item__img">
+                <img src="${fullCart.imageUrl}" alt="${fullCart.altTxt}">
+            </div>
+            <div class="cart__item__content">
+                <div class="cart__item__content__description">
+                    <h2>${fullCart.name}</h2>
+                    <span>Couleur : ${fullCart.color}</span>
+                    <p data-price="${fullCart.price}">Prix : ${fullCart.price} €</p>
+                </div>
+                <div class="cart__item__content__settings">
+                    <div class="cart__item__content__settings__quantity">
+                        <p>Quantité : </p>
+                        <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${fullCart.quantity}">
+                    </div>
+                    <div class="cart__item__content__settings__delete">
+                    <p class="deleteItem" data-id="${fullCart.id}" data-color="${fullCart.color}">Supprimer</p>
+                    </div>
+                </div>
+            </div>
+        </article>
+        `
+
+};
+
+
+
+
+
+/*
+let cartName = allProducts.name;
+let cartDescription = allProducts.description;
+let cartPrice = allProducts.price;
+let cartUrl = allProducts.imageUrl;
+let cartAltTxt = allProducts.altTxt;
+let cartId = allProducts._id.valueOf ;
+let cartQuantity = getPanier.quantity;
+let cartColor = getPanier.color;
+console.log(cartName, cartDescription, cartPrice, cartUrl,cartAltTxt, cartId, cartQuantity, cartColor);
+$fullCart = array(
+    "allProducts[i].name" = "selectedName",
+    "allProducts[i].description" = "selectedDescription",
+    "allProducts[i].price" = "selectedPrice",
+    "allProducts[i].imageUrl" = "selectedImageUrl",
+    "allProducts[i].altTxt" = "selectedAltTxt",
+    "allProducts[i]._id" = "selectedId",
+    "panier[i].quantity" = "quantity",
+    "panier[i].color" = "color",
+)
+print_r($array),
+console.log(fullCart);
+    
+
+
+
+    
+let fullCart = [];
+    for (let i = 0; i < allProducts.length; i++) {
+        let selectedName                    = allProducts[i].name;
+        let description			            = allProducts[i].description;
+        let price 							= allProducts[i].price;
+        let imageUrl 						= allProducts[i].imageUrl;
+        let altTxt 							= allProducts[i].altTxt;
+        let quantity                        = panier[i].quantity;
+        let color                           = panier[i].color;
+        let id                              = allProducts[i]._id;
+    fullCart.push(selectedName, description, price, imageUrl, altTxt, quantity, color,id)
+    console.log(fullCart);
+
+    
+    }
+    */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function getPanier() { 
     return localStorage.getItem('cart');
     }
